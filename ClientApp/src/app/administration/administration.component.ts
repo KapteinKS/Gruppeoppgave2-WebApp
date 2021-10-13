b87516353dce1a8754d5e2591e7881b768f34b25
KapteinKS <sander.toien@outlook.com>
2021-10-13 13:42:46 +0200

    some prep

---
+++ ClientApp/src/app/administration/administration.component.ts
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gruppeoppgave2_WebApp.ClientApp.src.app.administration
+{
+    public class administration
+    {
+    }
+}
