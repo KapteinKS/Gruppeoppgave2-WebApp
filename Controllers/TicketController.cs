93513be01b9dd100719517b9116a69df128f3c00
Kristian Wang <krkwang@hotmail.com>
2021-10-15 13:08:42 +0200

    Fix

--- Controllers/TicketController.cs
+++ Controllers/TicketController.cs
@@ -3,8 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Gruppeoppgave1_WebApp.DAL;
-using Gruppeoppgave1_WebApp.Model;
+using Gruppeoppgave2_WebApp.DAL;
+using Gruppeoppgave2_WebApp.Model;
 using Microsoft.Extensions.Logging;
 
 namespace Gruppeoppgave2_WebApp.Controllers
