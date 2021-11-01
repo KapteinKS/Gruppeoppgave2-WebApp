0b1583662c94f5da2368bfa5ba62d38719f41ded
karoline <s344089@oslomet.no>
2021-11-01 12:31:58 +0100

    validering for brukernavn og passord

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,20 @@ import { Component,OnInit } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  Skjema: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.Skjema = fb.group({
+      brukernavn: ["", Validators.required],
+      passord: ["", Validators.pattern("[0-9]{6,15}")]
+    });
+  }
+
+  onSubmit() {
+    console.log("Modellbasert skjema submitted");
+    console.log(this.Skjema);
+    console.log(this.Skjema.value.brukernavn);
+    console.log(this.Skjema.touched); 
+  }
 }
