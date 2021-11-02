95a2890b8d36f695c108e5ae3f1b2ec9f8997d04
Kristian Wang <krkwang@hotmail.com>
2021-11-02 12:10:58 +0100

    Session stuff, Bruker.cs model

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Gruppeoppgave2_WebApp
 {
@@ -26,6 +27,14 @@ namespace Gruppeoppgave2_WebApp
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = ".AdventureWorks.Session";
+                options.IdleTimeout = TimeSpan.FromSeconds(1800); // 30 minutter
+                options.Cookie.IsEssential = true;
+            });
+            services.AddDistributedMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +59,8 @@ namespace Gruppeoppgave2_WebApp
             }
 
             app.UseRouting();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
