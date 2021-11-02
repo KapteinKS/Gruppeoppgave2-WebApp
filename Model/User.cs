e150293a77e3405bc46fa8ca8ffdef11b5deb389
Kristian Wang <krkwang@hotmail.com>
2021-11-02 13:32:46 +0100

    Db User

--- Model/User.cs
+++ Model/User.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Gruppeoppgave2_WebApp.Model
 {
-    public class Bruker
+    public class User
     {
         private int ID { get; set; }
 
+        //[RegularExpression()]
         private string Username { get; set; }
-
+        //[RegularExpression()]
         private string Password { get; set; }
     }
 }
