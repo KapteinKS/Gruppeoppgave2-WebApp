9f16bea772c5cd74c8fb4020b8db498382dd8c3c
KapteinKS <sander.toien@outlook.com>
2021-11-03 14:54:32 +0100

    login work ts

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../user';
 
 @Component({
   //selector: 'router-outlet',
@@ -11,7 +14,7 @@ export class LoginComponent {
 
   Skjema: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private _http: HttpClient, private fb: FormBuilder, private router: Router) {
     this.Skjema = fb.group({
       brukernavn: ["", Validators.required],
       passord: ["", Validators.pattern("[0-9]{6,15}")]
@@ -23,9 +26,20 @@ export class LoginComponent {
     console.log(this.Skjema);
     console.log(this.Skjema.value.brukernavn);
     console.log(this.Skjema.touched);
+    this.logIn();
   }
 
   ngOnInit() {
 
+  }
+
+  logIn() {
+    var user = new User(); 
+    user.username = this.Skjema.value.brukernavn;
+    user.password = this.Skjema.value.passord;
+    this._http.post("api/departure/", user)
+      .subscribe(retur => {
+        this.router.navigate(['/administration']);
+      })
   }
 }
